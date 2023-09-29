dfd66ad94db7b88238dfd938076e85ac9f894346
niliara <nil.jimeno@estudiant.fjaverianas.com>
2023-09-29 13:35:36 +0200

    tween animations ver 0

--- tscn/card.gd
+++ tscn/card.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
 var soup = Vector2.ZERO
+var tween
+var tar
+var blocked = false
+
 var type
 var side
 var imp
@@ -27,7 +31,7 @@ func _ready():
 	rng = imp[2]
 	speed = imp[3]
 	
-	goSoup()
+	position=goSoup()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -35,7 +39,8 @@ func _process(_delta):
 
 
 func goSoup():
-	position = Vector2(soup.x-2, soup.y -1) * sb
+#	position = Vector2(soup.x-2, soup.y -1) * sb
+	return(Vector2(soup.x-2, soup.y -1) * sb)
 
 func move():
 	for i in range(0,speed):
@@ -43,22 +48,59 @@ func move():
 			gDel()
 			soup.x += 1*dir
 			gAdd()
-			goSoup()
 			if attack():
+				aMove()
+				aAttack()
 				break
+	aMove()
+
+func aMove():
+	tween.tween_property(self, "position", goSoup(), 0.2)
+
+func aAttack():
+	tween.tween_callback(tcomplete)
+	tween.tween_property(self, "position", goSoup() + Vector2(50, 0)*dir, 0.1)
+	tween.tween_interval(0.05)
+	tween.tween_property(self, "position", goSoup(), 0.1)
+	
+	
+
+func tcomplete():
+	
+	if is_instance_valid(tar):
+		tar.anima()
+
+func anima():
+	if hp <= 0:
+		tween = null
+		tween = create_tween().set_parallel(true).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+		blocked = true
+		
+		tween.tween_property(self, "position", goSoup()-Vector2(50*dir,0),0.6)
+		tween.tween_property(self, "modulate", Color(1,1,1,0),0.6)
+		
+		tween.set_parallel(false)
+		tween.tween_callback(queue_free)
+
 
 func act():
-	if not attack():
+	if blocked:
+		return(0)
+	tween = null
+	tween = create_tween().set_parallel(false).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	if attack():
+		aAttack()
+	else:
 		move()
 
 func attack():
 	for i in range(rng):
 		var xFound = soup.x+(1+i)*dir
 		if abs(xFound-2) >= 2:
-			Global.walls[side].dmg(att)
+			tar = Global.walls[side].dmg(att)
 			return(true)
 		else:
-			var tar = Global.b[soup.y][xFound]
+			tar = Global.b[soup.y][xFound]
 			if tar != null and tar.side != side:
 				tar.dmg(att)
 				return(true)
@@ -68,15 +110,13 @@ func attack():
 func dmg(vdmg):
 	hp -= vdmg
 	if hp <= 0:
-		var i = 0
 		var new_cs = []
 		for item in Global.c[side][soup.y]:
 			if item != self:
 				new_cs.append(item)
 			Global.c[side][soup.y] = new_cs
-			i+=1
 		gDel()
-		queue_free()
+#		queue_free()
 
 func gAdd():
 	Global.b[soup.y][soup.x] = self
